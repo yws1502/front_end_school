5de626009a8698550c7dc44d71a52f7fdbc2c536
Woosang <yws1502@naver.com>
2021-11-26 20:44:45 +0900

    20Day

---
+++ js/113_init.js
@@ -0,0 +1,27 @@
+// https://ko.javascript.info/ 소개
+// https://www.w3schools.com/ 소개
+
+// 자바스크립트로 출력하는 방법
+// let age = 10; //지금은 다루지 않습니다.
+// const pi = 3.141592; // 지금은 다루지 않습니다.
+var name = "사용자";
+var age = 20;
+
+document.getElementById('title').innerHTML = 'Welcome to JS100제';
+document.write(20 + 20);
+window.alert(name);
+console.log(age);
+// 한 줄 주석
+/* 주석 입니다. */
+
+var myVal1 = "asfsafasf" + "asfaf";
+var myVal2 = 1242154;
+var myVal3 = [1,2,3,4,5];
+var myVal4 = {var1 : 1, var2 : 2};
+var myVal5 = function() {};
+
+var myName = "조은강사님";
+var MyName = "호준강사님"
+
+window.alert(myName)
+window.alert(MyName)
---
+++ js/113_문자열.js
@@ -0,0 +1,36 @@
+// var 변수하나 = 20;
+// var 변수둘 = 10;
+// var 변수셋 = 2;
+
+// document.write('변수하나 : ', 변수하나);
+// document.write('<br>');
+// document.write('변수둘 : ', 변수둘);
+// document.write('<br>');
+// document.write('변수셋 : ', 변수셋);
+// document.write('<br>');
+// document.write('변수하나+변수둘 : ', 변수하나+변수둘);
+// document.write('<br>');
+// document.write('변수하나-변수둘 : ', 변수하나-변수둘);
+// document.write('<br>');
+// document.write('변수하나/변수둘 : ', 변수하나/변수둘);
+// document.write('<br>');
+// document.write('변수하나*변수둘 : ', 변수하나*변수둘);
+// document.write('<br>');
+// document.write('변수하나**변수셋 : ', 변수하나**변수셋);
+// document.write('<br>');
+// document.write('변수하나%변수셋 : ', 변수하나%변수셋);
+
+//ES6
+const 이름 = '이호준';
+const 소속 = '제주코딩베이스캠프 운영진입니다.'
+
+let 주소 = '서울';
+주소 = '제주';
+
+document.write(이름, '<br>');
+document.write(소속, '<br>');
+document.write(주소, '<br>');
+
+document.write(소속[2], '<br>');
+document.write(소속[3], '<br>');
+
