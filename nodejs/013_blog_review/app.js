4f2892f63d156d2a74be1c6c3e564248364c2405
Woosang <yws1502@naver.com>
2022-01-07 09:11:12 +0900

    learned nodeJs

---
+++ nodejs/013_blog_review/app.js
@@ -0,0 +1,72 @@
+const nunjucks = require('nunjucks');
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const blogRouter = require('./router/blog.js');
+const blogs = require("./database/dataBlog");
+const dateFilter = require('nunjucks-date-filter');
+const path = require('path');
+
+const app = express();
+app.set('view engine', 'html');
+
+경로 = path.join(path.join(__dirname + '/resource'), '/static');
+console.log(경로);
+
+app.use('/', express.static(경로));
+app.use('/blog', express.static(경로));
+
+// nunjucks 설정 코드
+let env = nunjucks.configure('resource/template', {
+    autoescape: true,
+    express: app,
+    watch: true
+});
+
+// 템플린 엔지 필터 추가 방법
+env.addFilter('date', dateFilter);
+
+
+app.use(express.json());
+app.use(express.urlencoded( {extended : false } ));
+
+
+app.use(helmet());
+app.use(cors());
+app.use(morgan('tiny'));
+
+
+app.get('/', (req, res, next) => {
+    const data = blogs(0, 2);
+    res.render('index.html', {data});
+});
+
+
+app.get('/about', (req, res, next) => {
+    res.render('about.html');
+});
+
+
+app.use('/blog', blogRouter);
+
+
+app.get('/contact', (req, res, next) => {
+    res.render('contact.html');
+});
+
+
+app.use((req, res, next) => {
+    res.sendStatus(404);
+})
+
+
+app.use((err, req, res, next) => {
+    console.log('애러났음!')
+    console.log(err);
+    res.sendStatus(500);
+})
+
+
+
+app.listen(8080);
---
+++ nodejs/013_blog_review/database/dataBlog.js
@@ -0,0 +1,31 @@
+let blogs = [
+  {
+    id : 1,
+    title : 'title1',
+    content : 'content1',
+    section : 'it',
+    viewCount : 0,
+    pubDate : new Date().toString(),
+    modDate : new Date().toString()
+  },
+  {
+    id : 2,
+    title : 'title2',
+    content : 'content2',
+    section : 'it',
+    viewCount : 0,
+    pubDate : new Date().toString(),
+    modDate : new Date().toString()
+  },
+  {
+    id : 3,
+    title : 'title3',
+    content : 'content3',
+    section : 'section3',
+    viewCount : 0,
+    pubDate : new Date().toString(),
+    modDate : new Date().toString()
+  },
+];
+
+module.exports = blogs;
